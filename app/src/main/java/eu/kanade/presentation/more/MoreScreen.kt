e5cefe7e4569fe9b37f7790a4e3a777c0d8acb36
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-03-29 20:18:09 +0200

    show "switch profile" conditionally

--- app/src/main/java/eu/kanade/presentation/more/MoreScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/MoreScreen.kt
@@ -14,6 +14,9 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.Storage
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalUriHandler
@@ -22,12 +25,15 @@ import eu.kanade.presentation.more.settings.widget.SwitchPreferenceWidget
 import eu.kanade.presentation.more.settings.widget.TextPreferenceWidget
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.ui.more.DownloadQueueState
+import mihon.feature.profiles.core.ProfileManager
 import tachiyomi.core.common.Constants
 import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.components.ScrollbarLazyColumn
 import tachiyomi.presentation.core.components.material.Scaffold
 import tachiyomi.presentation.core.i18n.pluralStringResource
 import tachiyomi.presentation.core.i18n.stringResource
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 @Composable
 fun MoreScreen(
@@ -45,6 +51,8 @@ fun MoreScreen(
     onClickAbout: () -> Unit,
 ) {
     val uriHandler = LocalUriHandler.current
+    val profileManager = remember { Injekt.get<ProfileManager>() }
+    val profiles by profileManager.visibleProfiles.collectAsState()
 
     Scaffold { contentPadding ->
         ScrollbarLazyColumn(
@@ -134,12 +142,14 @@ fun MoreScreen(
                     onPreferenceClick = onClickSettings,
                 )
             }
-            item {
-                TextPreferenceWidget(
-                    title = stringResource(MR.strings.profiles_switch_summary),
-                    icon = Icons.Outlined.AccountCircle,
-                    onPreferenceClick = onClickProfiles,
-                )
+            if (profiles.size > 1){
+                item {
+                    TextPreferenceWidget(
+                        title = stringResource(MR.strings.profiles_switch_summary),
+                        icon = Icons.Outlined.AccountCircle,
+                        onPreferenceClick = onClickProfiles,
+                    )
+                }
             }
             item {
                 TextPreferenceWidget(
